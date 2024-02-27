f4631f9e55dc0a66d9145de3ca6d18b11eab9136
Jaroslav Beran <jaroslav.beran@gmail.com>
2024-02-27 22:47:59 +0100

    Menus

--- svjis/articles/views.py
+++ svjis/articles/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 
 
 def main_view(request):
     return render(request, "main.html", {
+        'tray_menu_items': [
+            {'description': 'Články', 'link': reverse(main_view), 'active': True},
+            {'description': 'Redakce', 'link': reverse(redaction_view), 'active': False},
+        ],
         'aside_menu_name': 'Články',
         'aside_menu_items': [
             {'description': 'Všechny články', 'link': '/', 'active': True},
@@ -30,6 +35,10 @@ def user_logout(request):
 
 def redaction_view(request):
     return render(request, "redaction.html", {
+        'tray_menu_items': [
+            {'description': 'Články', 'link': reverse(main_view), 'active': False},
+            {'description': 'Redakce', 'link': reverse(redaction_view), 'active': True},
+        ],
         'aside_menu_name': 'Redakce',
         'aside_menu_items': [
             {'description': 'Články', 'link': '/', 'active': True},
