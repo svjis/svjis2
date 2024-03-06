faeff8e8d4c4085a0ba3a577512a99f66b3b09a7
Jaroslav Beran <jaroslav.beran@gmail.com>
2024-03-06 22:15:55 +0100

    Tests

--- svjis/articles/tests.py
+++ svjis/articles/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Article
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ArticleListTest(TestCase):
+
+    def test_anonymous_user_menu(self):
+        response = self.client.get(reverse('main'))
+        response_tray_menu = response.context['tray_menu_items']
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response_tray_menu), 1)
+        self.assertEqual(response_tray_menu[0]['description'], 'Articles')
