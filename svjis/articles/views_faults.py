09c1654b97eab709933f6b7cfbfbde2263c34ca9
Jaroslav Beran <jaroslav.beran@gmail.com>
2024-03-22 15:08:18 +0100

    Faults - pagination and search

--- svjis/articles/views_faults.py
+++ svjis/articles/views_faults.py
@@ -1,8 +1,11 @@
 from . import utils, forms, models
-from django.shortcuts import render, redirect, get_object_or_404
+from django.conf import settings
 from django.contrib.auth.decorators import permission_required
 from django.contrib import messages
+from django.core.paginator import Paginator, InvalidPage
+from django.db.models import Q
 from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
 from django.views.decorators.http import require_GET, require_POST
@@ -23,8 +26,41 @@ def get_side_menu(active_item, user):
 @require_GET
 def faults_all_view(request):
     fault_list = models.FaultReport.objects.all()
+
+    # Search
+    search = request.GET.get('search')
+    if search is not None and len(search) < 3:
+        messages.error(request, _("Search: Keyword '{}' is too short. Type at least 3 characters.").format(search))
+        search = None
+    if search is not None and len(search) > 100:
+            messages.error(request, _("Search: Keyword is too long. Type maximum of 100 characters."))
+            search = None
+    if search is not None:
+        fault_list = fault_list.filter(Q(subject__icontains=search) | Q(description__icontains=search))
+        header = _("Search results") + f": {search}"
+    else:
+        search = ''
+        header = _("Article")
+
+    # Paginator
+    is_paginated = len(fault_list) > getattr(settings, 'SVJIS_FAULTS_PAGE_SIZE', 10)
+    page = request.GET.get('page', 1)
+    paginator = Paginator(fault_list, per_page=getattr(settings, 'SVJIS_FAULTS_PAGE_SIZE', 10))
+    page_obj = paginator.get_page(page)
+    try:
+        fault_list = paginator.page(page)
+    except InvalidPage:
+        fault_list = paginator.page(paginator.num_pages)
+    page_parameter = '' if search == '' else f"search={search}"
+
     ctx = utils.get_context()
     ctx['aside_menu_name'] = _("Fault reporting")
+    ctx['header'] = header
+    ctx['is_paginated'] = is_paginated
+    ctx['page_obj'] = page_obj
+    ctx['page_parameter'] = page_parameter
+    ctx['search_endpoint'] = reverse(faults_all_view)
+    ctx['search'] = search
     ctx['aside_menu_items'] = get_side_menu('faults', request.user)
     ctx['tray_menu_items'] = utils.get_tray_menu('faults', request.user)
     ctx['object_list'] = fault_list
@@ -45,6 +81,7 @@ def fault_view(request, slug):
     ctx['aside_menu_items'] = get_side_menu('faults', request.user)
     ctx['tray_menu_items'] = utils.get_tray_menu('faults', request.user)
     ctx['obj'] = fault
+    ctx['search'] = request.GET.get('search', '')
     ctx['assets'] = utils.wrap_assets(fault.assets)
     ctx['asset_form'] = forms.FaultAssetForm
     return render(request, "fault.html", ctx)
