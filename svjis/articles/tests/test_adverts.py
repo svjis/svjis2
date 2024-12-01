f789c70ffe94ba268ce967ef071e233c07b36146
Jaroslav Beran <jaroslav.beran@gmail.com>
2024-12-01 11:47:50 +0100

    Advert test

--- svjis/articles/tests/test_adverts.py
+++ svjis/articles/tests/test_adverts.py
@@ -6,7 +6,7 @@ from .testdata import UserDataMixin
 
 class AdvertsTest(UserDataMixin, TestCase):
 
-    def create_advert(self, username, password, advert_form):
+    def create_advert(self, username, password, advert_form, status):
         logged_in = self.client.login(username=username, password=password)
         self.assertTrue(logged_in)
         response = self.client.post(
@@ -14,8 +14,20 @@ class AdvertsTest(UserDataMixin, TestCase):
             advert_form,
             follow=False,
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/adverts_list/')
+        if status == 302:
+            self.assertEqual(response.status_code, status)
+            self.assertEqual(response.url, '/adverts_list/')
+            response = self.client.get(reverse('adverts_list'), follow=True)
+            self.assertEqual(response.status_code, 200)
+
+            adverts = response.context['object_list']
+            self.assertEqual(len(adverts), 1)
+
+            advert = adverts[0]['advert']
+            return advert
+        else:
+            self.assertEqual(response.status_code, status)
+            return None
 
     def test_hide_adverts_of_deactivated_user(self):
         # create advert
@@ -31,6 +43,7 @@ class AdvertsTest(UserDataMixin, TestCase):
                 'email': 'test@test.com',
                 'published': True,
             },
+            302,
         )
 
         # advert is visible for other users
@@ -56,3 +69,68 @@ class AdvertsTest(UserDataMixin, TestCase):
 
         adverts = response.context['object_list']
         self.assertEqual(len(adverts), 0)
+
+    def test_advert_update(self):
+        advert = self.create_advert(
+            "petr",
+            "petr",
+            {
+                'pk': 0,
+                'type': 1,
+                'header': 'testing advert',
+                'body': 'testing advert body',
+                'phone': '123',
+                'email': 'test@test.com',
+                'published': True,
+            },
+            302,
+        )
+        self.assertEqual(advert.header, 'testing advert')
+
+        advert = self.create_advert(
+            "petr",
+            "petr",
+            {
+                'pk': advert.pk,
+                'type': 1,
+                'header': 'testing advert 2',
+                'body': 'testing advert body',
+                'phone': '123',
+                'email': 'test@test.com',
+                'published': True,
+            },
+            302,
+        )
+        self.assertEqual(advert.header, 'testing advert 2')
+
+    def test_advert_update_by_another_user(self):
+        advert = self.create_advert(
+            "petr",
+            "petr",
+            {
+                'pk': 0,
+                'type': 1,
+                'header': 'testing advert',
+                'body': 'testing advert body',
+                'phone': '123',
+                'email': 'test@test.com',
+                'published': True,
+            },
+            302,
+        )
+        self.assertEqual(advert.header, 'testing advert')
+
+        advert = self.create_advert(
+            "jiri",
+            "jiri",
+            {
+                'pk': advert.pk,
+                'type': 1,
+                'header': 'testing advert 2',
+                'body': 'testing advert body',
+                'phone': '123',
+                'email': 'test@test.com',
+                'published': True,
+            },
+            404,
+        )
