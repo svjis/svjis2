a096cf09e5e2633215db19256136e3be48615f43
Jaroslav Beran <jaroslav.beran@gmail.com>
2025-01-24 14:39:06 +0100

    svjis_setup

--- svjis/articles/tests/test_playwright.py
+++ svjis/articles/tests/test_playwright.py
@@ -1,5 +1,7 @@
 import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.core.management import call_command
+from ..utils import generate_password
 from playwright.sync_api import sync_playwright
 
 
@@ -10,6 +12,9 @@ class MyViewTests(StaticLiveServerTestCase):
         super().setUpClass()
         cls.playwright = sync_playwright().start()
         cls.browser = cls.playwright.chromium.launch()
+        cls.test_output_dir = 'playwright_output'
+        cls.user_password = generate_password(6)
+        call_command('svjis_setup', password=cls.user_password)
 
     @classmethod
     def tearDownClass(cls):
@@ -21,10 +26,10 @@ class MyViewTests(StaticLiveServerTestCase):
         page = self.browser.new_page()
         page.goto(f"{self.live_server_url}/")
         page.wait_for_selector('text=SVJIS2')
-        page.screenshot(path='playwright_output/home-page1.png')
-        page.fill('[id=login-input]', 'peter')
-        # page.fill('[id=password-input]', self.u_peter_password)
-        page.screenshot(path='playwright_output/home-page2.png')
+        page.screenshot(path=f'{self.test_output_dir}/home-page-01.png')
+        page.fill('[id=login-input]', 'admin')
+        page.fill('[id=password-input]', self.user_password)
+        page.screenshot(path=f'{self.test_output_dir}/home-page-02.png')
         page.click('id=login-submit')
-        page.screenshot(path='playwright_output/home-page3.png')
+        page.screenshot(path=f'{self.test_output_dir}/home-page-03.png')
         page.close()
