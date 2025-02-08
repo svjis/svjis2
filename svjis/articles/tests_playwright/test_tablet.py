b772243730dd0a5e036d310645b99a7172ab2a17
Jaroslav Beran <jaroslav.beran@gmail.com>
2025-02-08 10:23:51 +0100

    Playwright Screen resolutions

--- svjis/articles/tests_playwright/test_tablet.py
+++ svjis/articles/tests_playwright/test_tablet.py
@@ -3,6 +3,6 @@ from .test_desktop import DesktopTests
 
 class TabletTests(DesktopTests):
 
-    device_width = 878
-    device_height = 1000
+    device_width = 768
+    device_height = 915
     test_output_dir = 'playwright_output/tablet'
