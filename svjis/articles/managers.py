458d047cd159f07ce59c330b65a4c807cca90e5d
Jaroslav Beran <jaroslav.beran@gmail.com>
2025-08-14 17:02:39 +0200

    Fix of created

--- svjis/articles/managers.py
+++ svjis/articles/managers.py
@@ -7,35 +7,25 @@ if typing.TYPE_CHECKING:
 
 
 class FaultReportLogManager(models.Manager["FaultReportLog"]):
-    def log_actions(self, request, form, queryset, created: bool = False) -> None:
+    def log_actions(self, request, form, queryset) -> None:
         result = []
         for obj in queryset:
-            if created:
+            for change in form.changed_data:
+                if change == 'assigned_to_user':
+                    _type = self.model.TYPE_ASSIGNED
+                elif change == 'closed':
+                    if form.cleaned_data['closed']:
+                        _type = self.model.TYPE_CLOSED
+                    else:
+                        _type = self.model.TYPE_REOPENED
+                else:
+                    _type = self.model.TYPE_MODIFIED
                 result.append(
                     self.model(
                         fault_report=obj,
                         user=request.user,
                         resolver=obj.assigned_to_user,
-                        type=self.model.TYPE_CREATED,
+                        type=_type,
                     )
                 )
-            else:
-                for change in form.changed_data:
-                    if change == 'assigned_to_user':
-                        _type = self.model.TYPE_ASSIGNED
-                    elif change == 'closed':
-                        if form.cleaned_data['closed']:
-                            _type = self.model.TYPE_CLOSED
-                        else:
-                            _type = self.model.TYPE_REOPENED
-                    else:
-                        _type = self.model.TYPE_MODIFIED
-                    result.append(
-                        self.model(
-                            fault_report=obj,
-                            user=request.user,
-                            resolver=obj.assigned_to_user,
-                            type=_type,
-                        )
-                    )
         self.bulk_create(result)
--- svjis/articles/models.py
+++ svjis/articles/models.py
@@ -406,6 +406,9 @@ class FaultReport(models.Model):
             ("svjis_fault_resolver", "Can resolve fault"),
         )
 
+    def log_creating_ticket(self, user: User):
+        FaultReportLog.objects.create(fault_report=self, user=user, resolver=None, type=FaultReportLog.TYPE_CREATED)
+
     def log_taking_ticket(self, user: User):
         FaultReportLog.objects.create(fault_report=self, user=user, resolver=user, type=FaultReportLog.TYPE_ASSIGNED)
 
@@ -468,6 +471,7 @@ class FaultReportLog(models.Model):
         (TYPE_ASSIGNED, _("Assigned")),
         (TYPE_CLOSED, _("Closed")),
         (TYPE_REOPENED, _("Reopened")),
+        (TYPE_CREATED, _("Created")),
     )
 
     fault_report = models.ForeignKey(FaultReport, on_delete=models.CASCADE, related_name='logs')
