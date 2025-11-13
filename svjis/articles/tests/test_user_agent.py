883e10d41d741f4d770a73541c71005091f5b4de
Jaroslav Beran <jaroslav.beran@gmail.com>
2025-11-13 19:45:10 +0100

    UA tests

--- svjis/articles/tests/test_user_agent.py
+++ svjis/articles/tests/test_user_agent.py
@@ -5,23 +5,18 @@ from ..user_agent import get_browser, get_os
 class UserAgentTests(TestCase):
     cases = [
         {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",  # noqa
-            "browser": "Chrome",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",
+            "agent": "Mozilla/5.0 X11; Ubuntu; Linux x86_64; rv:126.0 Gecko/20100101 Firefox/126.0",  # noqa
             "browser": "Firefox",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
+            "os": {'os': 'Linux', 'platform': 'Desktop'},
         },
         {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36",  # noqa
-            "browser": "Chrome",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
+            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",  # noqa
+            "browser": "Safari",
+            "os": {'os': 'iOS', 'platform': 'Mobile'},
         },
         {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",  # noqa
-            "browser": "Edge",
+            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",  # noqa
+            "browser": "Firefox",
             "os": {'os': 'Windows', 'platform': 'Desktop'},
         },
         {
@@ -30,9 +25,44 @@ class UserAgentTests(TestCase):
             "os": {'os': 'Android', 'platform': 'Mobile'},
         },
         {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",  # noqa
+            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",  # noqa
+            "browser": "Chrome",
+            "os": {'os': 'Windows', 'platform': 'Desktop'},
+        },
+        {
+            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Mobile/15E148 Safari/604.1",  # noqa
             "browser": "Safari",
             "os": {'os': 'iOS', 'platform': 'Mobile'},
+        },
+        {
+            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
+            "browser": "Chrome",
+            "os": {'os': 'Windows', 'platform': 'Desktop'},
+        },
+        {
+            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",  # noqa
+            "browser": "Firefox",
+            "os": {'os': 'Windows', 'platform': 'Desktop'},
+        },
+        {
+            "agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.114 Safari/537.36",  # noqa
+            "browser": "Chrome",
+            "os": {'os': 'Windows', 'platform': 'Desktop'},
+        },
+        {
+            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_6_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1",  # noqa
+            "browser": "Safari",
+            "os": {'os': 'iOS', 'platform': 'Mobile'},
+        },
+        {
+            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1",  # noqa
+            "browser": "Safari",
+            "os": {'os': 'iOS', 'platform': 'Mobile'},
+        },
+        {
+            "agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
+            "browser": "Chrome",
+            "os": {'os': 'Linux', 'platform': 'Desktop'},
         },
     ]
 
