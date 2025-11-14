f7e8c0ca0892a976ffba1d79f6c9a3e2ba4c0de4
Jaroslav Beran <jaroslav.beran@gmail.com>
2025-11-14 10:38:42 +0100

    UA tests refa

--- svjis/articles/tests/test_user_agent.py
+++ svjis/articles/tests/test_user_agent.py
@@ -4,83 +4,89 @@ from ..user_agent import get_browser, get_os
 
 class UserAgentTests(TestCase):
     cases = [
+        # Firefox on Linux
         {
-            "agent": "Mozilla/5.0 X11; Ubuntu; Linux x86_64; rv:126.0 Gecko/20100101 Firefox/126.0",  # noqa
+            "agent": [
+                "Mozilla/5.0 X11; Ubuntu; Linux x86_64; rv:126.0 Gecko/20100101 Firefox/126.0",  # noqa
+            ],
             "browser": "Firefox",
             "os": {'os': 'Linux', 'platform': 'Desktop'},
         },
+        # Firefox on Windows
         {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",  # noqa
-            "browser": "Safari",
-            "os": {'os': 'iOS', 'platform': 'Mobile'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",  # noqa
+            "agent": [
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0",  # noqa
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",  # noqa
+            ],
             "browser": "Firefox",
             "os": {'os': 'Windows', 'platform': 'Desktop'},
         },
+        # Safari on iOS
         {
-            "agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36",  # noqa
+            "agent": [
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",  # noqa
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Mobile/15E148 Safari/604.1",  # noqa
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 18_6_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1",  # noqa
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1",  # noqa
+            ],
+            "browser": "Safari",
+            "os": {'os': 'iOS', 'platform': 'Mobile'},
+        },
+        # Chrome on Android
+        {
+            "agent": [
+                "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36",  # noqa
+            ],
             "browser": "Chrome",
             "os": {'os': 'Android', 'platform': 'Mobile'},
         },
+        # Chrome on Windows
         {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",  # noqa
+            "agent": [
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",  # noqa
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
+                "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.114 Safari/537.36",  # noqa
+            ],
             "browser": "Chrome",
             "os": {'os': 'Windows', 'platform': 'Desktop'},
         },
+        # Chrome on Linux
         {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0.1 Mobile/15E148 Safari/604.1",  # noqa
-            "browser": "Safari",
-            "os": {'os': 'iOS', 'platform': 'Mobile'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
-            "browser": "Chrome",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",  # noqa
-            "browser": "Firefox",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.114 Safari/537.36",  # noqa
-            "browser": "Chrome",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
-        },
-        {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_6_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.6 Mobile/15E148 Safari/604.1",  # noqa
-            "browser": "Safari",
-            "os": {'os': 'iOS', 'platform': 'Mobile'},
-        },
-        {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1",  # noqa
-            "browser": "Safari",
-            "os": {'os': 'iOS', 'platform': 'Mobile'},
-        },
-        {
-            "agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
+            "agent": [
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",  # noqa
+            ],
             "browser": "Chrome",
             "os": {'os': 'Linux', 'platform': 'Desktop'},
         },
+        # Chrome on MacOS
         {
-            "agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",  # noqa
-            "browser": "Edge",
-            "os": {'os': 'Windows', 'platform': 'Desktop'},
-        },
-        {
-            "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",  # noqa
+            "agent": [
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",  # noqa
+            ],
             "browser": "Chrome",
             "os": {'os': 'macOS', 'platform': 'Desktop'},
         },
+        # Edge on Windows
         {
-            "agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/29.0 Chrome/136.0.0.0 Mobile Safari/537.36",  # noqa
+            "agent": [
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",  # noqa
+            ],
+            "browser": "Edge",
+            "os": {'os': 'Windows', 'platform': 'Desktop'},
+        },
+        # Samsung Browser on Android
+        {
+            "agent": [
+                "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/29.0 Chrome/136.0.0.0 Mobile Safari/537.36",  # noqa
+            ],
             "browser": "Samsung Browser",
             "os": {'os': 'Android', 'platform': 'Mobile'},
         },
+        # CriOS on iOS
         {
-            "agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 26_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/142.0.7444.128 Mobile/15E148 Safari/604.1",  # noqa
+            "agent": [
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 26_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/142.0.7444.128 Mobile/15E148 Safari/604.1",  # noqa
+            ],
             "browser": "Crios",
             "os": {'os': 'iOS', 'platform': 'Mobile'},
         },
@@ -88,10 +94,12 @@ class UserAgentTests(TestCase):
 
     def test_browser(self):
         for case in self.cases:
-            result = get_browser(case.get("agent"))
-            self.assertEqual(result.get("browser", ""), case.get("browser", ""))
+            for a in case.get("agent"):
+                result = get_browser(a)
+                self.assertEqual(result.get("browser", ""), case.get("browser", ""))
 
     def test_os(self):
         for case in self.cases:
-            result = get_os(case.get("agent"))
-            self.assertEqual(result, case.get("os", ""))
+            for a in case.get("agent"):
+                result = get_os(a)
+                self.assertEqual(result, case.get("os", ""))
