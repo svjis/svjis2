d8c0d498d965a1904e4af7f697f33fd158a98847
Jaroslav Beran <jaroslav.beran@gmail.com>
2026-03-26 23:50:33 +0100

    webm extension

--- svjis/articles/model_utils.py
+++ svjis/articles/model_utils.py
@@ -77,7 +77,7 @@ def _slug_strip(value, separator='-'):
 def get_asset_icon(basename):
     document_icons = ['doc', 'docx', 'htm', 'html', 'pdf', 'pps', 'ppsx', 'txt', 'xls', 'xlsx', 'zip']
     picture_icons = ['gif', 'jpeg', 'jpg', 'png', 'webp']
-    video_icons = ['mpg', 'mpeg', 'mov', 'mp4', 'avi', 'wmv']
+    video_icons = ['mpg', 'mpeg', 'mov', 'mp4', 'avi', 'wmv', 'webm']
     _file_name, file_extension = os.path.splitext(basename)
     file_extension = file_extension[1:]
     if file_extension.lower() in document_icons:
