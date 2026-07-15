e33f9fdf11e1098a5a3b6fe7fd7bbf14d93c3b02
Jaroslav Beran <jaroslav.beran@gmail.com>
2026-07-15 12:54:01 +0200

    Do not log article view for articles which are not published

--- svjis/articles/views.py
+++ svjis/articles/views.py
@@ -212,8 +212,9 @@ def article_view(request, slug):
     if user.is_anonymous:
         user = None
 
-    ua = request.headers.get('user-agent', '')
-    models.ArticleLog.objects.create(article=article, user=user, user_agent=ua)
+    if article.published:
+        ua = request.headers.get('user-agent', '')
+        models.ArticleLog.objects.create(article=article, user=user, user_agent=ua)
 
     group_list = [g.name for g in article.visible_for_group.order_by('name')]
     if article.visible_for_all:
